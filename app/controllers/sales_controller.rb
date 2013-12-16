88319db74a734e81eb6b9b5f7f990a18386aede5
John Moon <johnmoonyy@gmail.com>
2013-12-16 14:28:06 -0500

    Finish sales page to display sales within the last 15 months

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,5 +1,6 @@
 class SalesController < ApplicationController
   def index
     @sales = Sale.all
+    @sale_within_last_6_months = Sale.last_6_months
   end
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,3 +1,7 @@
 class Sale < ActiveRecord::Base
   belongs_to :employee
+
+  def self.last_6_months
+    Sale.where(sale_date: 15.month.ago)
+  end
 end
