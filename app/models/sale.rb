18b5e572a7168807c2cdf21fa356393ad7b9fc47
John Moon <johnmoonyy@gmail.com>
2013-12-16 14:49:48 -0500

    Route link to each page and update sale views

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -2,6 +2,6 @@ class Sale < ActiveRecord::Base
   belongs_to :employee
 
   def self.last_6_months
-    Sale.where(sale_date: 15.month.ago)
+    Sale.where("sale_date >= ?", 15.months.ago).order(sale_date: :desc)
   end
 end
